4c33e344f1b5275e0fd34422efd6df0f99bf0645
jacquesjd <jacques.dancause@gmail.com>
2022-01-10 15:24:54 -0400

    Complete average function

---
+++ WPMAverager/WPMAverager/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace WPMAverager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(15, 38);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(237, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Touch Type Word Averager";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(12, 80);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(50, 13);
+            this.lblAverage.TabIndex = 2;
+            this.lblAverage.Text = "Average:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(255, 35);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "My Results:";
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(13, 124);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(317, 225);
+            this.lstResults.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 363);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstResults;
+    }
+}
+
---
+++ WPMAverager/WPMAverager/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WPMAverager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtInput.Text, out int output))
+            {
+                //error
+                return;
+            }
+
+            lstResults.Items.Add(output);
+
+            int total = 0;
+            foreach(int result in lstResults.Items)
+            {
+                total += result;
+            }
+            lblAverage.Text = $"Average: {total/lstResults.Items.Count}";
+            txtInput.Focus();
+        }
+    }
+}
